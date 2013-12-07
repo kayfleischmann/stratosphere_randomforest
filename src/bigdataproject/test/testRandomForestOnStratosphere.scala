6ea3987931d5e073a41caf586464790bf8c359d8
Kay Fleischmann <kay.fleischmann@gmx.de>
2013-12-07 15:16:28 +0100

    prototype1

---
+++ src/bigdataproject/test/testRandomForestOnStratosphere.scala
@@ -0,0 +1,30 @@
+package bigdataproject.test
+
+import eu.stratosphere.pact.client.LocalExecutor
+import bigdataproject.scala.eu.stratosphere.ml.randomforest.buildDecisionTree
+
+object testRandomForestOnStratosphere {
+  
+  def main(args: Array[String]) { 
+
+    // Write test input to temporary directory
+    val inputPath = "/home/kay/Desktop/small-trainingset"
+    		
+    // Output
+    val outputPath = "file:///home/kay/decisionTree_output"
+
+    val number_trees = ""+10
+
+    println("Reading input from " + inputPath)
+    println("Writing output to " + outputPath)
+
+    val plan = new buildDecisionTree().getPlan(inputPath, outputPath, number_trees )
+    val ex = new LocalExecutor()
+    ex.start()
+    val runtime = ex.executePlan(plan)
+    println("runtime:  " + runtime)
+    ex.stop();
+    
+    System.exit(0)
+  }
+}
