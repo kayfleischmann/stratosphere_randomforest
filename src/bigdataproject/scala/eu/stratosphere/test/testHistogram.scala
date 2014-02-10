8f4e7439a48a2e4598fba574be9c7c3831e185da
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-02-10 13:27:09 +0100

    testcases

--- src/bigdataproject/scala/eu/stratosphere/test/testHistogram.scala
+++ src/bigdataproject/scala/eu/stratosphere/test/testHistogram.scala
@@ -1,13 +1,12 @@
 package bigdataproject.scala.eu.stratosphere.test
 
-
 import bigdataproject.scala.eu.stratosphere.ml.randomforest.Histogram
 import org.junit.Test
 import org.junit.Assert._
 import org.scalatest.junit.JUnitSuite
+import scala.util.Random
 
 class testHistogram extends JUnitSuite  {
-
 
   @Test
   def testHistogram_paper = {
@@ -17,36 +16,28 @@ class testHistogram extends JUnitSuite  {
 	 h.update(10.0)
 	 h.update(16.0)
 	 h.update(36.0)
-
    assertTrue(  h.getBins.toList == List( (10.0,1), (16.0,1), (19.0,1), (23.0,1), (36.0,1) ) )
-	 System.out.println( h.getBins.toList.toString )
-	 h.update(2.0)
-	 System.out.println( h.getBins.toList.toString )
+
+   h.update(2.0)
+   assertTrue(  h.getBins.toList == List((2.0,1), (10.0,1), (17.5,2), (23.0,1), (36.0,1)) )
+
 	 h.update(9.0)
-	 System.out.println( h.getBins.toList.toString )
-	 
+   assertTrue(  h.getBins.toList == List((2.0,1), (9.5,2), (17.5,2), (23.0,1), (36.0,1)) )
+
 	 h.update(32.0)
-	 System.out.println( h.getBins.toList.toString )
+   assertTrue(  h.getBins.toList == List((2.0,1), (9.5,2), (17.5,2), (23.0,1), (34.0,2)) )
+
 	 h.update(30.0)
-	 System.out.println( h.getBins.toList.toString )
-	 h.update(45.0)
-	 h.update(45.0)
-	 h.update(45.0)
-	 System.out.println( h.getBins.toString )
-	 System.out.println( h.getBins.toString )
-	 
-	 
-	 System.out.println("uniform");
-	 System.out.println( h.uniform(5)  ) 
+   assertTrue(  h.getBins.toList == List((2.0,1), (9.5,2), (17.5,2), (23.0,1), (32.666666666666664,3)) )
   }
-  
+
+  @Test
   def testHistogram_serialization = {
-	 val strhistogram ="346;10;0.001636151149201403 302,0.09193899782135079 18,0.16823529411764707 10,0.28590102707749765 21,0.40620915032679733 12,0.5273725490196078 25,0.6425180598555209 19,0.7720965309200606 26,0.8930856553147575 38,0.991001880204138 146"
-
-	 System.out.println(strhistogram)
-	 val hh = Histogram.fromString(strhistogram)
-
-	 System.out.println(hh.toString.equals(strhistogram))
-	 System.out.println( hh.uniform(10) )
+    val h = new Histogram(2,10)
+    (0 until 20000).foreach({ x =>
+      val max = Random.nextDouble * (Random.nextInt(60) - 30)
+      h.update(max)
+    })
+    assertTrue( Histogram.fromString( h.toString ).toString == h.toString )
   }
 }
--- src/bigdataproject/scala/eu/stratosphere/test/testRandomForest.scala
+++ src/bigdataproject/scala/eu/stratosphere/test/testRandomForest.scala
@@ -1,22 +1,67 @@
 package bigdataproject.scala.eu.stratosphere.test
 
-import bigdataproject.scala.eu.stratosphere.ml.randomforest.DecisionTreeBuilder
+import org.junit.{After, Before, Test}
+import org.junit.Assert._
+import org.scalatest.junit.JUnitSuite
+import scala.util.Random
 import bigdataproject.scala.eu.stratosphere.ml.randomforest.RandomForestBuilder
-import bigdataproject.scala.eu.stratosphere.ml.randomforest.Histogram
+import org.junit.rules.TemporaryFolder
+import java.io.{File, PrintWriter}
 
-object testRandomForest {
- 
-  def main(args: Array[String]) {
+class testRandomForest extends JUnitSuite  {
+  var folder : TemporaryFolder = null
+  var numberTrees = 1
+  var testFolder : String = ""
+  var testEvaluationSet : String = ""
+  var testTrainingSet : String = ""
 
-    new RandomForestBuilder().build(
-      "/home/kay/rf/",
-      "/home/kay/Desktop/mnist8m.dataset",
-      "/home/kay/rf/rf_input_nodequeue",
-      "/home/kay/rf/rf_output",
-      "/home/kay/rf/rf_output_tree",
-      1
-    )
+  @Before
+  def prepare = {
+    System.out.println("prepare")
+    folder = new TemporaryFolder()
+    folder.create()
+    testFolder = folder.getRoot.toString
+    testEvaluationSet = testFolder+"mnist8m.test.evalset"
+    testTrainingSet = testFolder+"mnist8m.test.dataset"
 
   }
- 
+  @Test
+  def testBuilding {
+
+    val outTrainSet = new PrintWriter(new File(testTrainingSet));
+    outTrainSet.write(
+      io.Source.fromInputStream(getClass.getResourceAsStream("/bigdataproject/scala/eu/stratosphere/test/mnist8m.test.dataset")).mkString
+    )
+    outTrainSet.close()
+
+    val outEvalSet = new PrintWriter(new File(testEvaluationSet));
+    outEvalSet.write(
+      io.Source.fromInputStream(getClass.getResourceAsStream("/bigdataproject/scala/eu/stratosphere/test/mnist8m.test.dataset")).mkString
+    )
+    outEvalSet.close()
+
+    new RandomForestBuilder().build(
+      testFolder,
+      testTrainingSet,
+      testFolder+"rf_input_nodequeue",
+      testFolder+"rf_output",
+      testFolder+"rf_output_tree",
+      numberTrees
+    )
+  }
+
+  def testEvaluation {
+    new RandomForestBuilder().eval(
+      testTrainingSet,
+      testFolder+"rf_output_tree",
+      testFolder+"rf_output_evaluation"
+    )
+  }
+
+  @After
+  def cleanup = {
+    System.out.println("cleanup")
+    folder.delete()
+  }
+
 }
