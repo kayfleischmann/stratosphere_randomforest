866d15f3a42baf2893b926a31d38227b5dd07c24
Kay Fleischmann <kay.fleischmann@gmx.de>
2014-02-13 18:02:28 +0100

    RandomForestExecutor changed

--- src/bigdataproject/scala/eu/stratosphere/ml/randomforest/RandomForestExecutor.scala
+++ src/bigdataproject/scala/eu/stratosphere/ml/randomforest/RandomForestExecutor.scala
@@ -6,33 +6,45 @@ object RandomForestExecutor {
   def main(args: Array[String]) {
     if(args.length < 3){
       println("not enough parameters")
-      println("dest-path data-source build|eval number-trees [remoteJar remoteHost remotePort]")
+      println("[build number-trees¦eval] dest-path data-source  [remoteJar remoteHost remotePort]")
       return
     }
-
     val mode=args(0)
-    val path=args(1)
-    val data=args(2)
-    var remoteJar : String =null
-    var remoteHost : String =null
-    var remotePort : Int =0
 
     if(mode == "build"){
+      val path=args(1)
+      val data=args(2)
       val trees=args(3).toInt
 
-      if(args.length > 4)
-        remoteJar=args(3)
-      if(args.length > 5)
-        remoteHost=args(4)
-      if(args.length > 6)
-        remotePort=args(5).toInt
+      var remoteJar : String =null
+      var remoteHost : String =null
+      var remotePort : Int =0
 
+      if(args.length > 4)
+        remoteJar=args(4)
+      if(args.length > 5)
+        remoteHost=args(5)
+      if(args.length > 6)
+        remotePort=args(6).toInt
     new RandomForestBuilder(remoteJar,remoteHost,remotePort).build(
         path,
         data,
         trees
         )
     } else  if(mode == "eval"){
+      val path=args(1)
+      val data=args(2)
+
+      var remoteJar : String =null
+      var remoteHost : String =null
+      var remotePort : Int =0
+
+      if(args.length > 3)
+        remoteJar=args(3)
+      if(args.length > 4)
+        remoteHost=args(4)
+      if(args.length > 5)
+        remotePort=args(5).toInt
 
       new RandomForestBuilder().eval(
         data,
